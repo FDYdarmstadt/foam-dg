7ba819e3730ac87b057cfb7f103fbe5065ccc30d
Florian Kummer <floriankummer58@gmail.com>
2019-09-17 16:54:46 +0200

    created Burgers example

---
+++ src/dgBurgersEquation/dgBurgersEquation.C
@@ -0,0 +1,89 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.0
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    dgaplacianFoam
+
+Description
+    Solves a Laplace equation using the Discontinuous Galerkin Method
+
+Author
+    Hrvoje Jasak.  All rights reserved.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dgCFD.H"
+#include "fvCFD.H"
+#include "OFstream.H"
+//#include "gaussQuadrature.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+#   include "setRootCase.H"
+
+#   include "createTime.H"
+#   include "createPolyMesh.H"
+#   include "createDgMesh.H"
+#   include "createFvMesh.H"
+
+#   include "createFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nCalculating temperature distribution\n" << endl;
+
+    while(runTime < runTime.endTime())
+    {
+        runTime++;
+
+        Info<< "Time: " << runTime.timeName() << nl << endl;
+
+        dgScalarMatrix Te
+        (
+    //        dgm::dgLaplacian(T) +
+           dgm::dgDiv(U, T)
+        +
+           dgm::dgDdt(T)
+        );
+
+//        Info << nl << nl << "SOLVER:"<< nl << "MATRIX TE diag: " << Te.diag() << nl << endl;
+//        Info << "MATRIX TE upper: " << Te.upper() << nl << endl;
+//        Info << "MATRIX TE lowa: " << Te.lower() << nl << endl;
+//        Info << "MATRIX TE sors: " << Te.source() << nl << endl;
+
+        Te.solve();
+
+#       include "infoOut.H"
+
+        runTime.write();
+
+    }
+    Info<< "Test harness successfully finished." << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
