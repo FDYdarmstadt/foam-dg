36d54af60b4f4d1d0d2d1972fbc7b3a90f9f36bc
Dario <dario.klingenberg@web.de>
2023-02-24 12:54:30 +0100

    update bindings

--- src/discontinuousGalerkin/BoSSSwrapper/FixedOperators.h
+++ src/discontinuousGalerkin/BoSSSwrapper/FixedOperators.h
@@ -2,7 +2,7 @@
 // This is AUTO-GENERATED code created by the 
 // BoSSS External Language Binding code generator.
 // **Any manual changes are over-written if the code-generator is executed.**
-// Creation Date: 2/10/2023 4:19:36PM
+// Creation Date: 2/24/2023 12:53:12PM
 // ##########################################################################
 #pragma once
 namespace BoSSS
@@ -24,7 +24,7 @@ void _SetForeignPointer(void* ptr);
 void* _GetForeignPointer();
 BoSSS::Application::ExternalBinding::OpenFoamDGField* GetFlux();
 BoSSS::Application::ExternalBinding::OpenFoamDGField* GetMu();
-void CahnHilliard(BoSSS::Application::ExternalBinding::OpenFoamMatrix* mtx, BoSSS::Application::ExternalBinding::OpenFoamDGField* U, BoSSS::Application::ExternalBinding::OpenFoamPatchField* ptch, BoSSS::Application::ExternalBinding::OpenFoamPatchField* ptchU);
+void CahnHilliard(BoSSS::Application::ExternalBinding::OpenFoamMatrix* mtx, BoSSS::Application::ExternalBinding::OpenFoamDGField* U, BoSSS::Application::ExternalBinding::OpenFoamPatchField* ptch, BoSSS::Application::ExternalBinding::OpenFoamPatchField* ptchU, double deltaT);
 void Laplacian(BoSSS::Application::ExternalBinding::OpenFoamMatrix* mtx, BoSSS::Application::ExternalBinding::OpenFoamPatchField* ptch);
 private:
 void _InitMonoBindings();
